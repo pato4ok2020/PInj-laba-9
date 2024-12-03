f527979e215df87b2dbfa3d8de152ccb54df5d9f
pato4ok2020 <stepananikeev2019@gmail.com>
2024-12-03 20:30:54 +0300

    Добавлена структура данных о бегуне

--- PInj-laba-9/Project1/main.cpp
+++ PInj-laba-9/Project1/main.cpp
@@ -5,6 +5,20 @@ int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "Russian");
 	cout << "Аникеев Степан 24ПИнж1д вариант 1 Результаты марафона" << endl;
-	
+
+	//Структура данных о бегуне
+	struct Info_about_runners
+	{
+		int num;
+		char* surname[30];
+		int start_hours;
+		int start_minutes;
+		int start_seconds;
+		int finish_hours;
+		int finish_minutes;
+		int finish_seconds;
+		char* club[100];
+	};
+
 	return 0;
 }
